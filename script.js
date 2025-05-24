03f980d75c091c4199487d2966c1571daaa6f13e
Fajar025 <fajarcuy2004@gmail.com>
2025-05-25 00:06:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const songs = [
+  {
+    title: "Mangu",
+    artist: "Fourtwnty ft. Charita Utami",
+    likes: 125000,
+    plays: 27386203,
+    file: "audio/Fourtwnty_Mangu_ft_Charita_Utami_Official_Audio_mDw83aS4ALo_140.mp3",
+    image: "img/album1.jpg"
+  },
+  {
+    title: "Serana",
+    artist: "For Revenge",
+    likes: 374000,
+    plays: 107595270,
+    file: "audio/1fdb8bdb_Serana.mp3",
+    image: "img/album2.jpg"
+  },
+  {
+    title: "You'll be in my heart",
+    artist: "NIKI",
+    likes: 78000,
+    plays: 15927859,
+    file: "audio/4_5913744923743492256.mp3",
+    image: "img/album3.jpg"
+  }
+];
+
+const listEl = document.getElementById("song-list");
+const player = document.getElementById("audio-player");
+
+// Fungsi untuk format angka
+function formatNumber(num) {
+  if (num >= 1000000) return (num / 1000000).toFixed(1).replace('.0', '') + "Juta";
+  if (num >= 1000) return (num / 1000).toFixed(1).replace('.0', '') + "K";
+  return num;
+}
+let activeCard = null;
+
+songs.forEach((song, index) => {
+  const card = document.createElement("div");
+  card.className = "song-card";
+  card.innerHTML = `
+    <img src="${song.image}" alt="Cover ${song.title}" class="album-cover">
+    <div class="song-title">${song.title}</div>
+    <div class="song-meta">Penyanyi: ${song.artist}</div>
+    <div class="song-meta">❤️ ${formatNumber(song.likes)} &nbsp; ▶️ ${formatNumber(song.plays)}</div>
+  `;
+  card.addEventListener("click", () => {
+    player.src = song.file;
+    player.play();
+
+    if (activeCard) {
+      activeCard.classList.remove("active");
+    }
+    card.classList.add("active");
+    activeCard = card;
+  });
+  listEl.appendChild(card);
+});
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Ganti icon tombol
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️ Ganti Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Ganti Mode";
+  }
+});
+
+
+
